0aedd933989a1dde1584c2dce8cb019aa0af48f2
David López <davidlghellin@gmail.com>
2022-12-01 20:09:22 +0100

    [DWES] [PHP] [MYSQL] Borrado de Otto y ajuste botón

--- DWES/tema03/web/views/tienda/Tiendas.php
+++ DWES/tema03/web/views/tienda/Tiendas.php
@@ -25,24 +25,17 @@ $resultado = $conProyecto->query('SELECT id, nombre, tlf FROM tiendas', MYSQLI_U
                 printf('<td>%d</td>', $row[0]);
                 printf('<td>%s</td>', $row[1]);
                 printf('<td>%s</td>', $row[2]);
-                echo '<td><i class="fa-solid fa-user-pen"></i><i class="fa-solid fa-trash-can"></i></td>';
+                echo '<td>
+                        <button class="btn" href="./views/tienda/editar-tienda.php?p=1">
+                            <i class="fa-solid fa-user-pen"></i>
+                        </button>
+                        <button class="btn" title="Eliminar registro" onclick="fntDelTienda(1)">
+                            <i class="fa-solid fa-trash-can"></i>
+                        </button>
+                      </td>';
                 echo '</tr>';
             }
             ?>
-            <tr>
-                <th scope="row">3</th>
-                <td>3</td>
-                <td>Otto</td>
-                <td>789</td>
-                <td>
-                    <button class="btn" href="./views/tienda/editar-tienda.php?p=1">
-                        <i class="fa-solid fa-user-pen"></i>
-                    </button>
-                    <button class="btn" title="Eliminar registro" onclick="fntDelTienda(1)">
-                        <i class="fa-solid fa-trash-can"></i>
-                    </button>
-                </td>
-            </tr>
         </tbody>
     </table>
 </main>
